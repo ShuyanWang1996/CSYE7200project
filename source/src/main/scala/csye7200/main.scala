9e8ab58cecfd7145f8e29ff5b3ca82b06a9fba13
chongfan001900185 <fan.ch@husky.neu.edu>
2020-04-11 00:25:09 -0400

    fc: add writing to db

--- source/src/main/scala/csye7200/main.scala
+++ source/src/main/scala/csye7200/main.scala
@@ -1,23 +1,58 @@
 package csye7200
 
+import java.io.File
+
+import com.mongodb.spark.MongoSpark
 import csye7200.data.CsvParser
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkConf
 import org.apache.spark.sql.SparkSession
+import com.mongodb.spark.config._
+
+
+object analyze_process{
+  def get_fileList(path:String):List[File]={
+    val f = new File(path).listFiles.filter(f=>{f.isFile && f.getName.contains("csv")}).toList
+    f
+  }
+
+  def start()={
+    //init spark
+    val sparkConf = new SparkConf().setMaster("local").setAppName("csye7200")
+    val sparkSession = SparkSession.builder
+      .config(sparkConf)
+      .appName("csye_mongo")
+      // Configuration for writing in a Mongo collection
+      .config("spark.mongodb.output.uri", "mongodb://49.235.244.219:27017")
+      .config("spark.mongodb.output.database", "csye7200")
+      .config("spark.mongodb.output.collection", "daily_reports")
+      // Configuration for reading a Mongo collection
+      .config("spark.mongodb.input.uri", "mongodb://49.235.244.219:27017")
+      .config("spark.mongodb.input.database", "csye7200")
+      .config("spark.mongodb.input.collection", "daily_reports")
+      .getOrCreate()
+    //import all .csv in loop
+    val path="../data/JHU/csse_covid_19_data/csse_covid_19_daily_reports/"
+    get_fileList(path).foreach(f=>{
+        println(f.getPath)
+        //get DF from .csv
+        val base_df = CsvParser(f.getPath, sparkSession).parse()
+        base_df.show()
+        //write to MongoDB
+        MongoSpark.save(base_df.write.mode("overwrite"))
+    })
+  }
+}
+
+object simulation_process{
+  def start()={
+
+  }
+}
 
 object main {
   def main(args: Array[String]): Unit = {
     println("Entry")
-    //init spark
-    val sparkConf = new SparkConf().setMaster("local").setAppName("csye7200")
-    val sparkContext = new SparkContext(sparkConf)
-    val path="../data/JHU/csse_covid_19_data/csse_covid_19_daily_reports/04-07-2020.csv"
-    val sparkSession = SparkSession.builder
-      .config(sparkConf)
-      .appName("mycsye")
-      .getOrCreate()
-    //get DF from .csv
-    val base_df = CsvParser(path, sparkSession).parse()
-    base_df.show()
+    analyze_process.start()
   }
 }
