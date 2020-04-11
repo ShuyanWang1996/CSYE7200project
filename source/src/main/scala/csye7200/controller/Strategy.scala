6551f7ba4fab1bab02ff48245f616289a12bf993
chongfan001900185 <fan.ch@husky.neu.edu>
2020-04-11 16:23:56 -0400

    fc: add read data

--- source/src/main/scala/csye7200/controller/Strategy.scala
+++ source/src/main/scala/csye7200/controller/Strategy.scala
@@ -1,5 +1,56 @@
 package csye7200.controller
 
+import com.mongodb.spark.MongoSpark
+import csye7200.data.CsvParser
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkConf
+import org.apache.spark.sql.{DataFrame, Row, SparkSession}
+import com.mongodb.spark.config._
+import org.apache.spark.mllib.tree.DecisionTree
+import org.apache.spark.mllib.tree.model.DecisionTreeModel
+import org.apache.spark.mllib.util.MLUtils
+import org.apache.spark.sql.types.{IntegerType, StructField, StructType}
+import org.apache.spark.mllib.linalg.{Vector, Vectors}
+import org.apache.spark.mllib.regression.LabeledPoint
+
 class Strategy {
 
+}
+
+case class DTStrategy(df:DataFrame,sparks:SparkSession){
+  def start(): Unit = {
+    //add sequence
+    val schema = df.schema.add(StructField("index", IntegerType))
+    val dfRDD = df.rdd.zipWithIndex()
+    val rowRDD = dfRDD.map(f => Row.merge(f._1, Row(f._2)))
+    val df2 = sparks.createDataFrame(rowRDD, schema)
+    val targetInd = df2.columns.indexOf("index")
+    val featInd = List("total_confirmed").map(df.columns.indexOf(_))
+    val mrdd = df2.rdd.map(r => LabeledPoint(
+      r.getDouble(targetInd), // Get target value
+      // Map feature indices to values
+      Vectors.dense(featInd.map(r.getDouble(_)).toArray)
+    ))
+    // Split the data into training and test sets (30% held out for testing)
+    val splits = mrdd.randomSplit(Array(0.7, 0.3))
+    val (trainingData, testData) = (splits(0), splits(1))
+
+    // Train a DecisionTree model.
+    //  Empty categoricalFeaturesInfo indicates all features are continuous.
+    val categoricalFeaturesInfo = Map[Int, Int]()
+    val impurity = "variance"
+    val maxDepth = 5
+    val maxBins = 32
+
+    val model = DecisionTree.trainRegressor(mrdd, categoricalFeaturesInfo, impurity, maxDepth, maxBins)
+
+    // Evaluate model on test instances and compute test error
+    val labelsAndPredictions = testData.map { point =>
+      val prediction = model.predict(point.features)
+      (point.label, prediction)
+    }
+    val testMSE = labelsAndPredictions.map{ case (v, p) => math.pow(v - p, 2) }.mean()
+    println(s"Test Mean Squared Error = $testMSE")
+    println(s"Learned regression tree model:\n ${model.toDebugString}")
+  }
 }
--- source/src/main/scala/csye7200/main.scala
+++ source/src/main/scala/csye7200/main.scala
@@ -4,11 +4,12 @@ import java.io.File
 
 import com.mongodb.spark.MongoSpark
 import csye7200.data.CsvParser
+import csye7200.controller.{DTStrategy, Strategy}
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkConf
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.types._
 import com.mongodb.spark.config._
-
 
 object analyze_process{
   def get_fileList(path:String):List[File]={
@@ -25,22 +26,22 @@ object analyze_process{
       // Configuration for writing in a Mongo collection
       .config("spark.mongodb.output.uri", "mongodb://49.235.244.219:27017")
       .config("spark.mongodb.output.database", "csye7200")
-      .config("spark.mongodb.output.collection", "daily_reports")
-      // Configuration for reading a Mongo collection
-      .config("spark.mongodb.input.uri", "mongodb://49.235.244.219:27017")
-      .config("spark.mongodb.input.database", "csye7200")
-      .config("spark.mongodb.input.collection", "daily_reports")
+      .config("spark.mongodb.output.collection", "test")//daily_reports
       .getOrCreate()
     //import all .csv in loop
-    val path="../data/JHU/csse_covid_19_data/csse_covid_19_daily_reports/"
-    get_fileList(path).foreach(f=>{
-        println(f.getPath)
-        //get DF from .csv
-        val base_df = CsvParser(f.getPath, sparkSession).parse()
-        base_df.show()
-        //write to MongoDB
-        MongoSpark.save(base_df.write.mode("append"))
-    })
+//    val path="../data/JHU/csse_covid_19_data/csse_covid_19_daily_reports/"
+//    get_fileList(path).foreach(f=>{
+//      println(f.getPath)
+//      //get DF from .csv
+//      val base_df = CsvParser(f.getPath, sparkSession).parse()
+//      base_df.show()
+//      //write to MongoDB
+//      MongoSpark.save(base_df.write.mode("append"))
+//    })
+    //Read China data from mongoDB
+    val df_China = sparkSession.read.format("mongo").option("uri", "mongodb://49.235.244.219/csye7200.train_data_China").load()
+    DTStrategy(df_China,sparkSession).start()
+    val df_US = sparkSession.read.format("mongo").option("uri", "mongodb://49.235.244.219/csye7200.test_data_US").load()
   }
 }
 
